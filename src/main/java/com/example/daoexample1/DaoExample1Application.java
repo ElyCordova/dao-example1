33141527a8373652af31354a77e76db51aa90af0
ElyCordova <96073607+ElyCordova@users.noreply.github.com>
2023-10-31 19:29:39 -0600

    create proyect

---
+++ src/main/java/com/example/daoexample1/DaoExample1Application.java
@@ -0,0 +1,13 @@
+package com.example.daoexample1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaoExample1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DaoExample1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/daoexample1/DaoExample1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.daoexample1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaoExample1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
